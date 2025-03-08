9d38bdce4f8750692fa899706094feb9d68534ac
deeps2882 <deepaksekar03@gmail.com>
2025-03-08 15:15:26 +0530

    fix

--- src/router/allergy.ts
+++ src/router/allergy.ts
@@ -2,9 +2,9 @@
 
 import { Router } from "express";
 import { allergyController } from "../controller/allergy";
-import { authenticateJWT } from "../utility/helper";
+// import { authenticateJWT } from "../utility/helper";
 const allergyRouter = Router();
 
-allergyRouter.get("/", authenticateJWT, allergyController.getAllergy);
+allergyRouter.get("/", allergyController.getAllergy);
 
 export default allergyRouter;
